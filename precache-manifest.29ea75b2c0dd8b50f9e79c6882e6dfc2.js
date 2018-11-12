ac17079d4086c3df2272d66d17827c200f457b1a
Kehinde Egunjobi <kehindeegunjobi93@yahoo.com>
2018-11-12 15:34:31 +0100

    Updates

---
+++ precache-manifest.29ea75b2c0dd8b50f9e79c6882e6dfc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4643b1aa2b73156b95c",
+    "url": "/react-expense-tracker/static/js/runtime~main.d4643b1a.js"
+  },
+  {
+    "revision": "9bed46314265ea223e74",
+    "url": "/react-expense-tracker/static/js/main.9bed4631.chunk.js"
+  },
+  {
+    "revision": "9b706dd428e206da0578",
+    "url": "/react-expense-tracker/static/js/1.9b706dd4.chunk.js"
+  },
+  {
+    "revision": "9aac57fa28632c64104413498f4205be",
+    "url": "/react-expense-tracker/index.html"
+  }
+];
